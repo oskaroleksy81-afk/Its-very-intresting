79fd9c7051c481261875859021dca4927191e138
oskaroleksy81-afk <oskaroleksy81@gmail.com>
2026-01-12 18:33:36 +0100

    zdamie z kodland

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("test")
+import random
+
+
+znaki = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+dlugosc = int(input("Podaj długość hasła: "))
+
+haslo = ""
+
+for _ in range(dlugosc):
+    losowy_znak = random.choice(znaki)
+    haslo += losowy_znak
+
+print(haslo)
+
+
+ 
